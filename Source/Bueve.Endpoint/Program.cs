9375761d12b920d8740d13a3a57abe11ad72355b
I_SER_I <I_SER_I@hotmail.com>
2022-02-05 23:06:29 +0300

    feat: init endpoint

---
+++ Source/Bueve.Endpoint/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Blueve.TinderApi;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Blueve.Endpoint
+{
+    public class Program
+    {
+        public static String Token { get; set; }
+
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        static IHostBuilder CreateHostBuilder(String[] arguments)
+        {
+            Token = arguments.First();
+            return Host.CreateDefaultBuilder(arguments).ConfigureServices(ConfigureServices);
+        }
+
+        static void ConfigureServices(HostBuilderContext builder, IServiceCollection services)
+        {
+            services
+                .AddSingleton<ITinderClient>(new TinderClient(Token))
+                .AddHostedService<TinderService>();
+        }
+    }
+}

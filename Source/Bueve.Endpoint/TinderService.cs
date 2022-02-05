877738768b2d045f8f2556dd932a435b37e16f5c
I_SER_I <I_SER_I@hotmail.com>
2022-02-05 21:45:16 +0300

    feat: init TinderService

---
+++ Source/Bueve.Endpoint/TinderService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Blueve.TinderApi;
+using Microsoft.Extensions.Hosting;
+
+namespace Blueve.Endpoint
+{
+    public class TinderService : BackgroundService
+    {
+        private readonly IHostApplicationLifetime _applicationLifetime;
+        private readonly ITinderClient _tinderClient;
+
+        public TinderService(ITinderClient tinderClient, IHostApplicationLifetime applicationLifetime)
+        {
+            _applicationLifetime = applicationLifetime;
+            _tinderClient = tinderClient;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                
+            }
+            catch (TaskCanceledException exception) when (stoppingToken.IsCancellationRequested)
+            {
+                throw new Exception("Stop App: ", exception);
+            }
+        }
+    }
+}
